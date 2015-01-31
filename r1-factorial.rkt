0f71b4d7b536fc1549c8983f4821a1e80882ea1e
Chalermchon Samana <appcodev@gmail.com>
2015-02-01 02:39:05 +0700

    del old

--- r1-factorial.rkt
+++
@@ -1,8 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname r1-factorial) (read-case-sensitive #t) (teachpacks ((lib "docs.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "docs.rkt" "teachpack" "htdp")))))
-(define (n! n)
-  (if (= n 1) 
-      1
-      (* n (n! (- n 1)))))
-
